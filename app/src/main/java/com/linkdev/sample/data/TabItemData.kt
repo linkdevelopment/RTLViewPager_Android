ba233e33f0c5d87550adeabb535bdabc2838269d
wedadfcis <wedadabdelkreem@gmail.com>
2021-02-03 12:20:04 +0200

    add licence

--- app/src/main/java/com/linkdev/sample/data/TabItemData.kt
+++ app/src/main/java/com/linkdev/sample/data/TabItemData.kt
@@ -1,10 +1,26 @@
+/**
+Copyright (C) 2020 Link Development
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ **/
+
 package com.linkdev.sample.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class TabItemData(val tabTitle:String?= null, val itemDescription:String ? = null):
+data class TabItemData(val tabTitle: String? = null, val itemDescription: String? = null) :
     Parcelable {
 
 }
